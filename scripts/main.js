9a0ae708dd81c9395115ead1f98e5e0c5a718640
Dimitri Walters <me@dimi.io>
2017-08-02 21:51:17 -0400

    Display types at bottom to filter table

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
-  $('table').DataTable({
-    'sDom': 't',
-    'paging': false,
-  });
+  var t = $('table').clone();
+  function initTable() {
+    $('table').DataTable({
+      'sDom': 't',
+      'paging': false,
+    });
+  }
   $('a').click(function() {
     window.open($(this).attr('href'), '_blank');
     return false;
   });
+  $(':checkbox').change(function() {
+    var t2 = t.clone();
+    $('.dataTables_wrapper').remove();
+    $('body').prepend(t2);
+    initTable();
+    t = t2;
+    var type = $(this).data('type');
+    if (type) {
+      console.log('hi');
+      $('.row-' + $(this).data('type')).toggle();
+    } else {
+      console.log($('.check-all').is(':checked'));
+      $('tbody tr').toggle($('.check-all').is(':checked'));
+      $(':checkbox').prop('checked', $('.check-all').is(':checked'));
+    }
+  });
+  initTable();
 });
